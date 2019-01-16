0ac831a7262e9f3f632592c958ced60e99c1c49d
潘文涛 <pwt980408@hotmail.com>
2019-01-16 15:26:10 +0800

    http.js axios 封装更新

--- src/utils/http.js
+++ src/utils/http.js
@@ -5,6 +5,8 @@ import globe from '../../static/config'
 axios.interceptors.request.use(config => {
   // 这里可以加一些动作, 比如来个进度条开始动作
   config.url = globe.allurl + config.url
+  // config.headers.common.Authorization = '90b231da-25d6-41f8-adea-0a2f5d912c4f'
+  console.log(config)
   return config
 }, error => {
   return Promise.reject(error)
@@ -23,6 +25,7 @@ function checkStatus (response) {
     return response
     // 如果不需要除了data之外的数据，可以直接 return response.data
   }
+  console.log(response)
   // 异常状态下，把错误信息返回去
   return {
     status: -404,
@@ -43,42 +46,46 @@ function checkCode (res) {
 
 export default {
   post (url, data) {
-    return axios({
-      method: 'post',
-      url,
-      data: qs.stringify(data),
-      timeout: 10000,
-      headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-      }
-    }).then(
-      (response) => {
-        return checkStatus(response)
-      }
-    ).then(
-      (res) => {
-        return checkCode(res)
-      }
-    )
+    return new Promise((resolve, reject) => {
+      axios({
+        method: 'post',
+        url,
+        data: qs.stringify(data),
+        timeout: 10000,
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest',
+          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        }
+      }).then(
+        (response) => {
+          resolve(checkStatus(response))
+        }
+      ).then(
+        (res) => {
+          reject(checkCode(res))
+        }
+      )
+    })
   },
   get (url, params) {
-    return axios({
-      method: 'get',
-      url,
-      params, // get 请求时带的参数
-      timeout: 10000,
-      headers: {
-        'X-Requested-With': 'XMLHttpRequest'
-      }
-    }).then(
-      (response) => {
-        return checkStatus(response)
-      }
-    ).then(
-      (res) => {
-        return checkCode(res)
-      }
-    )
+    return new Promise((resolve, reject) => {
+      axios({
+        method: 'get',
+        url,
+        params, // get 请求时带的参数
+        timeout: 10000,
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest'
+        }
+      }).then(
+        (response) => {
+          return checkStatus(response)
+        }
+      ).then(
+        (res) => {
+          return checkCode(res)
+        }
+      )
+    })
   }
 }
